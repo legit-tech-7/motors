47f436237a83eb29ce01a71882a03b2a6935a1ad
joshua <joshuachukwuemeka52@gmail.com>
2025-08-23 01:49:59 +0100

    Fix allowed hosts for Render

--- web/settings.py
+++ web/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+lsvi2_a^n&&i_m5t%zxt4so&l07$99r_eww2%^qqe%#5!_pge
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
